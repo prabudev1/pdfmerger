6096cc638da1097cb82a981edc3a9c89061e0d5a
prabudev1 <prabu_dev1@yahoo.co.in>
2019-10-20 21:02:34 +0530

    UI Page

--- src/main/resources/static/assets/js/main.js
+++ src/main/resources/static/assets/js/main.js
@@ -1,3 +1,28 @@
-/**
- * 
- */
+(function($) {
+	$("#sortable1").sortable({
+		revert : true
+	});
+	$("#sortable2").sortable({
+		revert : true
+	});
+	$("ul, li").disableSelection();	
+	$(".dz-message-bottom").hide();
+	
+//	var myDropzone = new Dropzone("#my-dropzone-element-id");
+//	  myDropzone.on("addedfile", function(file) {
+//		  
+//	  });
+	$("#mergepdf").click(function(){
+		//alert(Dropzone.options.myDropzoneElementId.getAcceptedFiles().length);
+	});
+})(jQuery);
+
+
+function fileRemove(thisObj) {
+	$(thisObj).parent().remove();
+	if ($(".dropzone .dz-preview").length == 0 ) {
+		$(".dropzone.dz-started .dz-message").show();
+		$(".dz-message-bottom").hide();
+	}
+	
+}
